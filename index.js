715f57ec05b42c3479b5576adf8ef15acb4c596e
Arthur Verschaeve <contact@arthurverschaeve.be>
2015-05-02 17:19:49 +0200

    fixup extra `var`

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ module.exports = function (rgbString) {
 	var rgb = {};
 
 	// First remove all whitespace, then parse the rgb string
-	var rgbString = rgbString.replace(/ /g, '');
+	rgbString = rgbString.replace(/ /g, '');
 
 	rgbString.replace(/rgb\((\d+),(\d+),(\d+)\)/, function (rgbString, r, g, b) {
 		rgb.red = Number(r);
